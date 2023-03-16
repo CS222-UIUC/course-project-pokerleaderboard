2155659a329c9256464b03f602c5156943b13e13
Danish Singh Sethi <dssethi2@illinois.edu>
2023-03-15 22:33:56 -0500

    created frontend for end game page

---
+++ frontend/src/EndGame.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./assets/styles/bulma.min.css";
+import "./assets/styles/EndGame.css"
+
+
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
+export const EndGame = () => { 
+    const players = [
+        "Daniel",
+        "Yejun",
+        "Justin",
+        "Pedro"
+    ];
+
+    let rows = [];
+
+    players.forEach((name, index) => {
+        rows.push(
+            <>
+                <label htmlFor={`${name}-amount`}>
+                    {name}
+                </label>
+                <input className="input" type="number" id={`${name}-amount`} placeholder={`Enter final amount for ${name}`} />
+            </>
+        );
+    });
+
+    return (
+        <section className="section">
+            <div className="container">
+                <h1 className="title">End Game</h1>
+                <form className="form" action="/leaderboard/start_game/" method="post">
+                    <input type="hidden" name="csrfmiddlewaretoken" />
+                    <div className="field">
+                        <label className="label">Enter the Final Amount for each Player:</label>
+                        <div className="control input-amounts-div">
+                            { rows }
+                        </div>
+                    </div>
+                    <div className="field">
+                        <div className="control">
+                        <button className="button is-success" type="submit">End Game</button>
+                        </div>
+                    </div>
+                    </form>
+            </div>
+        </section>
+    )
+}
